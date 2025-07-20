e56a6dc4a57f2c18415aba9a98ca80b6aae70017
sriram-24 <sriram1999m@gmail.com>
2025-07-20 20:49:58 +0530

    chore: cleanup and comments

--- server_stats.sh
+++ server_stats.sh
@@ -1,37 +1,26 @@
 #!/usr/bin/env sh
 
-convertKBToGB()
-{
-    param="$1"
-    float_value=$(( $1 * 100 / 1048576 ))
-    int_value=$((float_value/100))
-    frac_value=$((float_value % 100))
-    GB="$int_value.$frac_value"
-    echo $GB
-}
-
-convertToPercentage(){
-    part="$1"
-    whole="$2"
-    percentage=$((part * 100 / whole))
-    echo "$percentage"
-}
-
+# prints cpu usage by using the top program. 
 echo "--------CPU USAGE---------"
 echo "$(top -b -n1 | grep 'Cpu' | awk '{print $2+$4 }')%"
 
+#prints RAM usage by using the free program.
 echo "--------RAM USAGE---------"
 echo "$(free -m | grep 'Mem' | awk '{printf "Total: %d MB \nUsed: %d MB \nFree: %d MB", $2, $3, $4}')"
 
+#prints disk usage by using the df program.
 echo "--------DISK USAGE---------"
 echo "$(df -k --total | grep 'total' | awk '{print $5}')"
 
+#prints top 5 CPU usage by using the ps program.
 echo "--------Top 5 Cpu process--------"
 echo "$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -6)"
 
+#prints top 5 RAM usage by using the ps program.
 echo "--------Top 5 RAM process--------"
 echo "$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -6)"
 
+#prints logged in users by using the who program.
 echo "--------Logged In Users----------"
 echo "$(who)"
 
